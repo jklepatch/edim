d0d4a533e671421a616909299cf61bdada1eeb96
Julien Klepatch <julien@julienklepatch.com>
2018-10-10 11:50:48 +0800

    Add a few stuffs to unit3

--- unit3/finished/app/js/index.js
+++ unit3/finished/app/js/index.js
@@ -1,17 +1,29 @@
-import $ from 'jquery'; 
-import config from './config'; 
-import App from './lib/app';
+//import $ from 'jquery'; 
+//import config from './config'; 
+//import App from './lib/app';
+//
+//$(() => { 
+//  const app = new App(config); 
+//  app.setup()
+//  //.then(() => { 
+//  //  return app.init(); 
+//  //}) 
+//  .then(() => { 
+//    console.log('Wallet Dapp loaded!'); 
+//  }) 
+//  .catch((error) => {
+//    console.error(`Ooops... something went wrong: ${error}`);
+//  });
+//});
 
-$(() => { 
-  const app = new App(config); 
-  app.setup()
-  //.then(() => { 
-  //  return app.init(); 
-  //}) 
-  .then(() => { 
-    console.log('Wallet Dapp loaded!'); 
-  }) 
-  .catch((error) => {
-    console.error(`Ooops... something went wrong: ${error}`);
-  });
-});
+import React from 'react';
+import { render } from 'react-dom';
+import eth from './ethereum';
+import App from './components/App';
+
+eth.getAccounts.then(accounts => {
+  render(
+    <App web3={eth.web3} accounts={accounts} wallet={eth.wallet}/>,
+    document.getElementById('app')
+  );
+})
