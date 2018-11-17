41ba1692d014034f6b398483c088fe1c9c1044fc
Julien Klepatch <julien@julienklepatch.com>
2018-11-17 17:37:26 +0800

    Forgot import

--- unit3/module5/app/App.js
+++ unit3/module5/app/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import Header from './layout/Header';
 import Main from './layout/Main';
 import Footer from './layout/Footer';
-import eth from './ethereum';
+import { web3, wallet } from './ethereum';
 
 class App extends Component {
   async componentDidMount() {
     const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
     const result = wallet.methods.createTransfer().call();
     console.log(result);
-    console.log(accounts);
   }
   render() {
     return (
