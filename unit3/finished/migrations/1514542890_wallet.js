3b15aa450c4145f7c534ef59bc6cad22529d9819
Julien Klepatch <julien@julienklepatch.com>
2018-11-18 15:56:41 +0800

    Remove old code

--- unit3/finished/migrations/1514542890_wallet.js
+++
@@ -1,5 +0,0 @@
-const Wallet = artifacts.require("./Wallet.sol");
-
-module.exports = function(deployer, _network, accounts) {
-  deployer.deploy(Wallet, 2, [accounts[0], accounts[1], accounts[2]]);
-};
