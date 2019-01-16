e4f39c74941ad537e9425e3f96e50f11f5f46259
Julien Klepatch <julien@julienklepatch.com>
2019-01-17 01:42:30 +0800

    Add WETH

--- unit5/finished2/migrations/1541845366_dex.js
+++ unit5/finished2/migrations/1541845366_dex.js
@@ -1,33 +1,39 @@
 const Dex = artifacts.require('Dex');
+const WETH = artifacts.require('WETH');
 const EOS = artifacts.require('EOS');
 const OMG = artifacts.require('OMG');
 
 
 module.exports = function(deployer, _network, accounts) {
   deployer.deploy([
-    [EOS, {from: accounts[1]}],
-    [OMG, {from: accounts[2]}],
+    [WETH, {from: accounts[1]}],
+    [EOS, {from: accounts[2]}],
+    [OMG, {from: accounts[3]}]
   ])
   .then(() => {
     return deployer.deploy(
       Dex, 
-      ['EOS', 'OMG'],
-      [EOS.address, OMG.address],
+      ['WETH', 'EOS', 'OMG'],
+      [WETH.address, EOS.address, OMG.address],
       {from: accounts[0]}
     );
   })
   .then(() => {
     return Promise.all([
-      EOS.at(EOS.address).transfer(accounts[3], 1000, {from: accounts[1]}),
-      OMG.at(OMG.address).transfer(accounts[3], 1000, {from: accounts[2]}),
-      EOS.at(EOS.address).transfer(accounts[4], 1000, {from: accounts[1]}),
-      OMG.at(OMG.address).transfer(accounts[4], 1000, {from: accounts[2]}),
+      WETH.at(WETH.address).transfer(accounts[3], 1000, {from: accounts[1]}),
+      EOS.at(EOS.address).transfer(accounts[3], 1000, {from: accounts[2]}),
+      OMG.at(OMG.address).transfer(accounts[3], 1000, {from: accounts[3]}),
+      WETH.at(WETH.address).transfer(accounts[4], 1000, {from: accounts[1]}),
+      EOS.at(EOS.address).transfer(accounts[4], 1000, {from: accounts[2]}),
+      OMG.at(OMG.address).transfer(accounts[4], 1000, {from: accounts[3]}),
     ]);
   })
   .then(() => {
     return Promise.all([
+      WETH.at(WETH.address).approve(Dex.address, 1000, {from: accounts[3]}),
       EOS.at(EOS.address).approve(Dex.address, 1000, {from: accounts[3]}),
       OMG.at(OMG.address).approve(Dex.address, 1000, {from: accounts[3]}),
+      WETH.at(WETH.address).approve(Dex.address, 1000, {from: accounts[4]}),
       EOS.at(EOS.address).approve(Dex.address, 1000, {from: accounts[4]}),
       OMG.at(OMG.address).approve(Dex.address, 1000, {from: accounts[4]}),
     ]);
