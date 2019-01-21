a9a067ce977c618bf99e546ddc2fb10e822ba475
Julien Klepatch <julien@julienklepatch.com>
2019-01-21 17:19:09 +0800

    Reorg folders

--- unit5/finished/contracts/Migrations.sol
+++ unit5/finished/contracts/Migrations.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.2;
+/* solhint-disable */
+pragma solidity ^0.5.0;
 
 contract Migrations {
   address public owner;
@@ -8,7 +9,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  function Migrations() public {
+  constructor() public {
     owner = msg.sender;
   }
 
--- unit5/finished/src/config.js
+++
@@ -1,5 +0,0 @@
-const tokens = ["EOS", "OMG"];
-
-export {
-  tokens,
-};
